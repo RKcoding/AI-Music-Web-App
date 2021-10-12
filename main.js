65d50962c7131e6dab76eef248dec7716d7947b6
RKcoding <73450077+RKcoding@users.noreply.github.com>
2021-10-11 19:04:28 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 music1 = "";
 music2 = "";
+LeftWristX = 0;
+LeftWristY = 0;
+RightWristX = 0;
+RightWristY = 0;
 
 function preload() {
     music1 = loadSound("music.mp3");
@@ -11,8 +15,26 @@ function setup() {
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses);
 }
 
 function draw() {
     image(video, 0, 0, 300, 300);
+}
+
+function modelLoaded() {
+    console.log("PoseNet Is Initialized!");
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        LeftWristX = results[0].pose.leftWrist.x;
+        LeftWristY = results[0].pose.leftWrist.y;
+        console.log("LeftWristX = " + LeftWristX + "LeftWristY = " + LeftWristY);
+        RightWristX = results[0].pose.rightWrist.x;
+        RightWristY = results[0].pose.rightWrist.y;
+        console.log("RightWristX = " + RightWristX + "RightWristY = " + RightWristY);
+    }
 }
